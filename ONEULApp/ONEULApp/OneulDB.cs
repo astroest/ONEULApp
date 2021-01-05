dd7ed1b2e85c50d1ecbd3cff295cf82fd61b78ff
hes <es001206@naver.com>
2021-01-06 02:56:59 +0900

    초기 커밋.

---
+++ ONEULApp/ONEULApp/OneulDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ONEULApp
+{
+    class OneulDB
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public DateTime Time
+        {
+            get;
+            set;
+        }
+
+        public string Memo
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ ONEULApp/ONEULApp/Views/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ONEULApp.Views;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ONEULApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new WritePage()));
+        }
+    }
+}
